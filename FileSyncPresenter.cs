7731eb797af83dc40bf9aff3e233ccb5ca657510
Olesya251925 <118814113+Olesya251925@users.noreply.github.com>
2023-06-17 12:45:47 +0700

    Update FileSyncPresenter.cs

--- FileSyncPresenter.cs
+++ FileSyncPresenter.cs
@@ -13,13 +13,18 @@ namespace FileSyncApp
         {
             this.view = view;
             this.model = model;
-
+            
+            // Подписываемся на событие SyncRequested представления
             this.view.SyncRequested += View_SyncRequested;
         }
-
+        
+        // Обработчик события SyncRequested представления
         private void View_SyncRequested(object sender, EventArgs e)
         {
+             // Проверяем изменения в модели
             string status = model.CheckChanges();
+            
+             // Отображаем статус синхронизации в представлении
             view.DisplayStatus(status);
         }
     }
